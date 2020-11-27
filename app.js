90ef7d98f404a6040bcb29cfcd3248f7fa6b5143
cophung <phungtheco1@gmail.com>
2020-11-27 22:50:26 +0700

    fix heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 var indexRouter = require("./routes/index");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use("/api", indexRouter);
 
