69735809f5f2ef98f30848b1d73a4205365fede9
cophung <phungtheco1@gmail.com>
2020-11-30 08:26:18 +0700

    update models folder

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,9 @@ const models = require("../models/index");
 module.exports = {
   getRoutes: function (req, res) {
     const routes = models.handleRoutes();
-    res.status(201).send({
+    res.status(200).send({
       success: "true",
-      message: routes,
+      routes,
     });
   },
 };
--- models/index.js
+++ models/index.js
@@ -2,24 +2,22 @@ const fs = require("fs");
 
 const vrp = require("../util/vrp");
 
+const readFileJson = (path) => {
+  return JSON.parse(
+    fs.readFileSync(path, {
+      encoding: "utf8",
+      flag: "r",
+    })
+  );
+};
+
+const ids = readFileJson("./db/id.json");
+const db = readFileJson("./db/db.json");
+
+const cars = { capacity: 20 };
+
+const routes = vrp.handleVrp(ids, db, 7, cars);
+
 module.exports = {
-  handleRoutes: function () {
-    const cars = { capacity: 20 };
-
-    const ids = JSON.parse(
-      fs.readFileSync("./db/id.json", {
-        encoding: "utf8",
-        flag: "r",
-      })
-    );
-    const db = JSON.parse(
-      fs.readFileSync("./db/db.json", {
-        encoding: "utf8",
-        flag: "r",
-      })
-    );
-
-    const routes = vrp.handleVrp(ids, db, 7, cars);
-    return routes;
-  },
+  handleRoutes: () => routes,
 };
