67a7e28a3e29d111f742cd0b4d8facb84ba19312
cophung <phungtheco1@gmail.com>
2020-11-30 14:33:22 +0700

    install cors package

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let dataRoutes = require("./routes/createData");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use("/api", indexRouter);
 app.use("/createData", dataRoutes);
 
